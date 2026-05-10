de1681353f87bca952a4abec5a71c1062ce14e08
derektorres <torrespaniaguaderekervey@gmail.com>
2026-05-10 10:41:45 -0700

    se creo la estructura de la aplicacion

---
+++ src/main/java/itson/edu/mx/entities/Tarjeta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itson.edu.mx.entities;
+
+/**
+ *
+ * @author torre
+ */
+public class Tarjeta {
+    
+}
